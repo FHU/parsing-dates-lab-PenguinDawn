a4a5ee4343928425cd2b18c8b5fc6322095d8db0
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2024-02-01 10:16:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,54 @@
 #as a string
 #January -> 1 (as a string)
 #YOU MAY USE THIS FUNCTION IF YOU WANT TO OR YOU MAY REMOVE IT
-def parse_month(month):
-    pass
+
 
 #REMOVE PASS AND FIX THIS FUNCTION
 #parse_date function should return the date formated as MM/DD/YYYY
 #DO NOT REMOVE THIS FUNCTION
 def parse_date(user_string):
-    pass
+    listing = user_string.split()
+    print(listing)
+    
+    #month
+    dictOfMonth = {
+        "January": "01",
+        "February": "02",
+        "March": "03",
+        "April": "04",
+        "May": "05",
+        "June": "06",
+        "July": "07",
+        "August": "08",
+        "September": "09",
+        "October": "10",
+        "November": "11",
+        "December": "12"
+    }
+
+    part1 = dictOfMonth[listing[0]]
+
+    #days
+    listing2 = listing[1]
+    part2 = ""
+    if len(listing2) == 3:
+        part2 = listing2[0:2]
+    elif len(listing2) == 2:
+        part2 = "0"
+        part2 += listing2[0]
+
+    #year
+    part3 = listing[2]
+
+    end = part1 + "/" + part2 + "/" + part3
+    return end
+
+    
 
 #REMOVE PASS AND YOUR CODE GOES HERE
 if __name__ == '__main__':
-    pass
+    user_str = input()
+    while user_str != "-1":
+        print(parse_date(user_str))
+        user_str = input()
+        
